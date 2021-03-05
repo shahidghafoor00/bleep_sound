4c9067869b5df96a557f07637a317b0bfe774363
Shahid Ghafoor <shahid@macbook.local>
2021-03-05 17:57:35 +0500

    add sound

--- Bleep/ViewController.swift
+++ Bleep/ViewController.swift
@@ -6,14 +6,29 @@
 //
 
 import UIKit
+import AVFoundation
+
 
 class ViewController: UIViewController {
 
+    var audioPlayer: AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let sound = Bundle.main.path(forResource: "beep", ofType: "mp3")
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: sound!))
+        } catch {
+            print(error)
+        }
     }
 
-
+    
+    @IBAction func playBeep(_ sender: Any) {
+        audioPlayer?.play()
+    }
+    
 }
 
