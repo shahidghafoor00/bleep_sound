c8439611eccb5b6d98b5aa81d8b302d3ff8d97dd
Shahid Ghafoor <shahid@macbook.local>
2021-03-13 16:13:09 +0500

    fix ads issue

--- Bleep/ViewController.swift
+++ Bleep/ViewController.swift
@@ -7,11 +7,16 @@
 
 import UIKit
 import AVFoundation
+import GoogleMobileAds
 
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, GADBannerViewDelegate, GADFullScreenContentDelegate {
 
     var audioPlayer: AVAudioPlayer?
+    
+    // ads...
+    var bannerView: GADBannerView!
+    private var interstitial: GADInterstitialAd?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,6 +28,29 @@ class ViewController: UIViewController {
         } catch {
             print(error)
         }
+        
+        
+        // banner
+        bannerView = GADBannerView(adSize: kGADAdSizeBanner)
+        addBannerViewToView(bannerView)
+        bannerView.adUnitID = "ca-app-pub-3940256099942544/2934735716"
+        bannerView.rootViewController = self
+        bannerView.load(GADRequest())
+        bannerView.delegate = self
+        
+        
+        // Interstitial
+        let request = GADRequest()
+            GADInterstitialAd.load(withAdUnitID:"ca-app-pub-3940256099942544/4411468910",
+                                        request: request,
+                              completionHandler: { [self] ad, error in
+                                if let error = error {
+                                  print("Failed to load interstitial ad with error: \(error.localizedDescription)")
+                                  return
+                                }
+                                interstitial = ad
+                              }
+            )
     }
 
     
@@ -32,6 +60,44 @@ class ViewController: UIViewController {
         } else {
             audioPlayer?.play()
         }
+    }
+    
+    func addBannerViewToView(_ bannerView: GADBannerView) {
+      bannerView.translatesAutoresizingMaskIntoConstraints = false
+      view.addSubview(bannerView)
+      view.addConstraints(
+        [NSLayoutConstraint(item: bannerView,
+                            attribute: .bottom,
+                            relatedBy: .equal,
+                            toItem: bottomLayoutGuide,
+                            attribute: .top,
+                            multiplier: 1,
+                            constant: 0),
+         NSLayoutConstraint(item: bannerView,
+                            attribute: .centerX,
+                            relatedBy: .equal,
+                            toItem: view,
+                            attribute: .centerX,
+                            multiplier: 1,
+                            constant: 0)
+        ])
+     }
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        if interstitial != nil {
+            interstitial?.present(fromRootViewController: self)
+          } else {
+            print("Ad wasn't ready")
+          }
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        if interstitial != nil {
+            interstitial?.present(fromRootViewController: self)
+          } else {
+            print("Ad wasn't ready")
+          }
     }
     
 }
